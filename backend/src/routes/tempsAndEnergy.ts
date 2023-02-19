5dd45cfee7244914e11c8de10eb89f7987d8041c
Joe Dawes <jdawes22@gmail.com>
2023-02-19 17:41:45 +0000

    Add dependencies in router'

--- backend/src/routes/tempsAndEnergy.ts
+++ backend/src/routes/tempsAndEnergy.ts
@@ -2,10 +2,21 @@ import { Router } from "express";
 import { getEnergyRepository } from "../repositories/energyRepository";
 import { getEnergyService } from "../services/energyService";
 import { getTempsAndEnergyController } from "../controllers/tempsAndEnergyController";
+import { getTemperatureService } from "../services/tempertatureService";
+import { getTemperatureRepository } from "../repositories/temperatureRepository";
 
+const temperatureRepository = getTemperatureRepository();
+const temperatureService = getTemperatureService({ temperatureRepository })
+const energyRepository = getEnergyRepository();
+const energyService = getEnergyService({ energyRepository })
+
+const tempsAndEnergyController = getTempsAndEnergyController({
+  temperatureService,
+  energyService
+})
 
 const router = Router();
 
-router.get('/', );
+router.get('/', tempsAndEnergyController.getTempsAndEnergy);
 
 export default router;
