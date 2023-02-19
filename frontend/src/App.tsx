d20dec9a719337570ec760cb35f00ebc13a5f63d
Joe Dawes <jdawes22@gmail.com>
2023-02-19 21:17:12 +0000

    Add highcharts and axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import * as Highcharts from 'highcharts';
+import highchartsAccessibility from 'highcharts/modules/accessibility';
+import HighchartsReact from 'highcharts-react-official';
+import { useEffect, useState } from 'react';
 
-function App() {
+highchartsAccessibility(Highcharts);
+
+interface Temperature {
+  Date: Date;
+  AverageTemperature: number;
+  AverageHumidity: number;
+}
+
+interface Energy {
+  Timestamp: Date;
+  Consumption: number;
+  anomaly: boolean;
+}
+
+interface ChartData {
+  temperatureData: Temperature[];
+  energyData: Energy[];
+}
+
+const App = () => {
+  const [chartOptions, setChartOptions] = useState<Highcharts.Options>({
+    chart: {
+      type: 'line',
+      height: (2 / 4) * 100 + '%',
+    },
+    title: {
+      text: 'Energy consumption and temperature data',
+      align: 'left',
+    },
+    yAxis: [
+      {
+        title: {
+          text: 'Energy Consumption',
+          textAlign: 'left',
+        },
+      },
+      {
+        title: {
+          text: 'Temperature data',
+          textAlign: 'right',
+        },
+        opposite: true,
+      },
+    ],
+    xAxis: {
+      title: {
+        text: 'Date Range',
+      },
+      accessibility: {
+        rangeDescription: 'Date range',
+      },
+    },
+    series: [
+      {
+        yAxis: 0,
+        name: 'Consumption',
+        type: 'line',
+      },
+    ],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get<ChartData>(
+        'http://localhost:8080/tempsAndEnergy'
+      );
+      setChartOptions({
+        xAxis: {
+          categories: data?.energyData.map((energy) =>
+            energy?.Timestamp.toString()
+          ),
+        },
+        series: [
+          {
+            name: 'Consumption',
+            type: 'line',
+            data: data?.energyData.map((energy) => {
+              return {
+                y: energy.Consumption,
+                marker: {
+                  enabled: energy.anomaly,
+                  fillColor: 'red'
+                },
+                dataLabels: {
+                  enabled: energy.anomaly
+                }
+              }
+            })
+          },
+          {
+            yAxis: 1,
+            name: 'Humidity',
+            type: 'line',
+            data: data?.temperatureData.map(
+              (temperature) => temperature.AverageHumidity
+            ),
+          },
+          {
+            yAxis: 1,
+            name: 'Temperature',
+            type: 'line',
+            data: data?.temperatureData.map(
+              (temperature) => temperature.AverageTemperature
+            ),
+          },
+        ],
+      });
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
     </div>
   );
-}
+};
 
 export default App;
